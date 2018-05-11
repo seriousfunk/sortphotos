ab1d61ac384b095cac09098d295515e6ff2467d7
Matt Muro <mm@watchcity.com>
2018-05-11 11:14:03 -0400

    display exif info for all jpgs in directory

--- sortphotos.js
+++ sortphotos.js
@@ -1,24 +1,58 @@
-const fs 	= require('fs');
-const path 	= require('path');
-const os 	= require("os");
-const exif 	= require('exif');
- 
+const fs 		= require('fs');
+const path 		= require('path');
+const os 		= require("os");
+const ExifImage = require('exif').ExifImage;
+
+const logFile = fs.createWriteStream('./sortphotos.log');
+
 if (process.argv.length <= 2) {
 
     console.log(os.EOL + "\x1b[1m \x1b[31m Usage: \x1b[0m node " + path.basename(__filename, '.js') + " path/to/directory" + os.EOL);
-    process.exit(-1);
+    process.exit(1);
 }
- 
+
 var dirPath = process.argv[2];
  
 fs.readdir(dirPath, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        var file = dirPath + '/' + items[i];
+    for (let i=0; i<items.length; i++) {
+        let file = dirPath + '/' + items[i];
         console.log("Start: " + file);
  
         fs.stat(file, function(err, stats) {
-            console.log(file);
-            console.log(stats["size"]);
-        });
+
+			if ('.jpg' == path.extname(file)) {
+
+				try {
+				    new ExifImage({ image : file }, function (error, exifData) {
+				        if (error) {
+				            console.log('ExifImage Error: ' + file + " " + error.message);
+						    // logFile.end();
+				    		process.exit(1);
+				    	}
+				        else {
+
+							// logFile.write("-------------------------------");
+							// logFile.write(file);
+							// logFile.write(exifData);
+
+				        	console.log(os.EOL + "\x1b[1m \x1b[33m ------------------------------- \x1b[0m")
+				        	console.log(os.EOL + "\x1b[1m \x1b[33m " + file + "   \x1b[0m")
+				            console.log(exifData); // Do something with your data!
+				        }
+				    });
+				} catch (error) {
+				    console.log('Error: ' + error.message);
+				    // logFile.end();
+				    process.exit(2);
+				}
+
+	            console.log(file);
+	            console.log(stats["size"]);
+			}    
+
+	     });
+
     }
+
 });
+
