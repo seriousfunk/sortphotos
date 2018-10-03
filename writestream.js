fbd62f25f528b410188c653cfba0bd20b18b771c
Matt Muro <mm@watchcity.com>
2018-10-03 19:28:02 -0400

    writestream working

--- writestream.js
+++ writestream.js
@@ -4,22 +4,22 @@ const mkdirp  = require('mkdirp')
 const chalk   = require('chalk')
 
 let logFile = path.normalize('./my/logs/writestream.log')
+let logPath = path.normalize(path.dirname(logFile))
 
-async function setupLogging (logFile) {
-  await mkdirp(logFile, function (err) {
-    if (err) {
-      console.log(chalk`${os.EOL}{bgRed  mkdirp Error: } {red ${err} }`)
-    }
-  })
-  return new Promise(resolve => {
-    resolve(fs.createWriteStream(logFile, {flags: 'w'}))
-  })
-}
+console.log(`Log file: ${logFile}`)
+console.log(`Log path: ${logPath}`)
 
-(async () => {
-  let writeStream = await setupLogging(logFile)
-  writeStream.write('alpha \n');
-  writeStream.write('beta\n');
-  writeStream.end()
-})();
+mkdirp.sync(logPath, function (err) {
+  if (err) {
+    console.log(chalk`${os.EOL}{bgRed  mkdirp Error: } {red ${err} }`)
+  }
+})
+
+let writeStream = fs.createWriteStream(logFile, {flags: 'w'})
+writeStream.on('error', function(e){console.log(e)})
+
+writeStream.write('super \n');
+writeStream.write('man\n');
+writeStream.end()
+
 
